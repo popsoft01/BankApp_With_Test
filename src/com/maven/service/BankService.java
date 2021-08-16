b5d2da4efaeced9ee7a3994cda96c8e2dca7788e
popsoft01 <popoolatunde52@gmail.com>
2021-08-16 22:07:26 +0100

    bank application

---
+++ src/com/maven/service/BankService.java
@@ -0,0 +1,31 @@
+package com.maven.service;
+
+public class BankService {
+    private static long currentBVN =0;
+    private static long currentAccount =0;
+
+    public static long generateBVN(){
+        currentBVN++;
+        return currentBVN;
+    }
+    public static long generateAccountNumber(){
+        currentAccount++;
+        return currentAccount;
+    }
+
+    public static long getCurrentBVN() {
+        return currentBVN;
+    }
+
+    private static void setCurrentBVN(long currentBVN) {
+        BankService.currentBVN = currentBVN;
+    }
+
+    public static long getCurrentAccountNumber() {
+        return currentAccount;
+    }
+
+    private static void setCurrentAccount(long currentAccount) {
+        BankService.currentAccount = currentAccount;
+    }
+}
