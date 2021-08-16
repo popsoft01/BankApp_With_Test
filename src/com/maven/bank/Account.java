5bec8b23c5ca1d633ec1c6a7eaaaf1fc14813118
popsoft01 <popoolatunde52@gmail.com>
2021-08-16 22:07:43 +0100

    bank application

---
+++ src/com/maven/bank/Account.java
@@ -0,0 +1,46 @@
+package com.maven.bank;
+
+import com.maven.dataStore.AccountType;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private long accountNumber;
+    private BigDecimal balance;
+    private AccountType type;
+    private String accountId;
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+
+}
